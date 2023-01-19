a1df3bfe4cc7c4cc753b9a8616835b9b23d03664
Kevin <kelvinngechu@gmail.com>
2023-01-20 01:08:38 +0300

    home file

---
+++ information-flow/src/Home.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>
+        We are Web Developers from Moringa School
+      </h1>
+    </div>
+  );
+}
+export default Home;
