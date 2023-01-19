d3b4a630b9f1afcbef897131d0d9448a2f195796
levy <lnjiru43@gmail.com>
2023-01-19 20:05:17 +0300

    added to components folder

--- information-flow/src/components/app/App.js
+++ information-flow/src/components/app/App.js
@@ -1,10 +1,12 @@
-import Navbar from './Navbar/Navbar';
+import Navbar from '../Navbar/Navbar';
 import './App.css';
+import About from '../about/About';
 
 function App() {
   return (
     <div>
     <Navbar/>
+    <About/>
     </div>
   );
 }
